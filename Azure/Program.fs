e4e26032c4463890039545e28bc0d7c927ac160a
Andreas Lengkeek <andreaslengkeek@ssw.com.au>
2020-12-23 14:46:11 +1100

    Add resource prefix to arm

--- Azure/Program.fs
+++ Azure/Program.fs
@@ -4,12 +4,18 @@ open Farmer.Builders
 
 let audience = Environment.GetEnvironmentVariable  "AUTH0_AUDIENCE"
 let issuer = Environment.GetEnvironmentVariable  "AUTH0_ISSUER"
+let mutable namePrefix = Environment.GetEnvironmentVariable "AZURE_RG_PREFIX"
+
+if isNull namePrefix then
+    namePrefix <- "sswrules-local"
+
+printfn "name prefix: %s" namePrefix
 
 // 1. Create a cosmos db
 printfn "Creating CosmosDb"
 let myCosmosDb = cosmosDb {
-    name "sswrules-cosmosdb-staging"
-    account_name "sswrulescosmosdb-staging"
+    name (namePrefix + "-cosmosdb")
+    account_name (namePrefix + "-cosmosaccount")
     throughput 400<CosmosDb.RU>
     failover_policy CosmosDb.NoFailover
     consistency_policy (CosmosDb.Session)
@@ -18,8 +24,7 @@ let myCosmosDb = cosmosDb {
 // 2. Create a Functions App
 printfn "Creating Functions App"
 let myFunctions = functions {
-    name "sswrules-functions-staging"
-    zip_deploy "myFunctionsFolder"
+    name (namePrefix + "-functions")
     setting "OidcApiAuthorizationSettings:Audience" audience
     setting "OidcApiAuthorizationSettings:IssuerUrl" issuer
     setting "CosmosDb:Account" myCosmosDb.Endpoint
@@ -39,7 +44,7 @@ deployment
 
 printfn "All done! The template has been saved to arm-template.json"
 
-let json =
-    deployment.Template
-    |> Writer.toJson
-printfn "%s" json
+// let json =
+//     deployment.Template
+//     |> Writer.toJson
+// printfn "%s" json
