3af23868491e1a03caba4d1ec54b5ed8abc377db
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2021-01-04 11:10:41 +1000

    Add authentication to reactions (#8)
    * ✨  Add auth to like dislike functions

--- Functions/Functions/GetLikesDislikesFunction.cs
+++ Functions/Functions/GetLikesDislikesFunction.cs
@@ -1,13 +1,16 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using OidcApiAuthorization.Abstractions;
+using OidcApiAuthorization.Models;
 
 namespace SSW.Rules.Functions
 {
@@ -15,41 +18,63 @@ namespace SSW.Rules.Functions
     {
         private readonly RulesDbContext _dbContext;
 
-        public GetLikesDislikesFunction(RulesDbContext dbContext) 
+        public GetLikesDislikesFunction(RulesDbContext dbContext)
         {
             _dbContext = dbContext;
         }
-        
+
         [FunctionName("GetLikesDislikesFunction")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogWarning($"HTTP trigger function {nameof(GetLikesDislikesFunction)} received a request.");
 
             string RuleGuid = req.Query["rule_guid"];
+            string UserId = req.Query["user_id"];
 
-            if (RuleGuid == null) {
-                return new JsonResult(new {
+            if (RuleGuid == null)
+            {
+                return new JsonResult(new
+                {
                     error = true,
                     message = "Missing RuleGuid param",
                 });
             }
 
             var likes = await _dbContext.LikeDislikes.Query(q => q.Where(w => w.RuleGuid == RuleGuid));
-
+            if (likes.Count() == 0)
+            {
+                return new JsonResult(new
+                {
+                    error = true,
+                    message = "Could not find results for rule id: " + RuleGuid,
+                });
+            }
+            
             var results = likes
                 .GroupBy(l => l.Type)
-                .Select(g => new {
+                .Select(g => new
+                {
                     Type = g.Key,
                     Count = g.Count()
                 });
 
-            return new JsonResult(new {
+            ReactionType? userStatus = null;
+            if (!string.IsNullOrEmpty(UserId))
+            {
+                var userReaction = likes.Where(w => w.UserId == UserId).FirstOrDefault();
+                userStatus = userReaction?.Type ?? null;
+                log.LogInformation("Found reaction for user: '{0}' reaction: '{1}'", UserId, userStatus);
+            }
+
+            return new JsonResult(new
+            {
                 error = false,
                 message = "",
                 likeCount = results.Where(r => r.Type == ReactionType.Like).FirstOrDefault()?.Count ?? 0,
-                dislikeCount = results.Where(r => r.Type == ReactionType.Dislike).FirstOrDefault()?.Count ?? 0
+                dislikeCount = results.Where(r => r.Type == ReactionType.Dislike).FirstOrDefault()?.Count ?? 0,
+                userStatus = userStatus
             });
         }
     }
--- Functions/Functions/LikeDislikeFunction.cs
+++ Functions/Functions/LikeDislikeFunction.cs
@@ -8,23 +8,37 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using OidcApiAuthorization.Abstractions;
+using OidcApiAuthorization.Models;
 using SSW.Rules.Functions;
 
-namespace SSW.Rules.Functions {
-
-    public class LikeDislikeFunction {
-
+namespace SSW.Rules.Functions
+{
+    public class LikeDislikeFunction
+    {
         private readonly RulesDbContext _dbContext;
+        private readonly IApiAuthorization _apiAuthorization;
 
-        public LikeDislikeFunction(RulesDbContext dbContext) {
+        public LikeDislikeFunction(RulesDbContext dbContext, IApiAuthorization apiAuthorization)
+        {
             _dbContext = dbContext;
+            _apiAuthorization = apiAuthorization;
         }
 
         [FunctionName("LikeDislikeFunction")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-            ILogger log) {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            ILogger log)
+        {
+            //Add Auth
+            ApiAuthorizationResult authorizationResult = await _apiAuthorization.AuthorizeAsync(req.Headers);
+
+            if (authorizationResult.Failed)
+            {
+                log.LogWarning(authorizationResult.FailureReason);
+                return new UnauthorizedResult();
+            }
+            log.LogWarning($"HTTP trigger function {nameof(LikeDislikeFunction)} request is authorized.");
 
             LikeDislike data;
 
@@ -32,10 +46,12 @@ namespace SSW.Rules.Functions {
             data = JsonConvert.DeserializeObject<LikeDislike>(requestBody);
 
             // 1. validate data
-            if (data == null) {
-                return new JsonResult(new {
+            if (data == null)
+            {
+                return new JsonResult(new
+                {
                     error = true,
-                    message = "Request body is empty or in the wrong format",
+                    message = "Request body is empty",
                 });
             }
 
@@ -44,25 +60,32 @@ namespace SSW.Rules.Functions {
             var model = results.FirstOrDefault();
             log.LogInformation($"reactions on same rule by same user: {results.Count()}");
 
-            if (model != null) {
-                log.LogInformation("Reaction already exists for user {0}", model.UserId);
-
-                if (model.Type != data.Type) {
-                    model.Type = data.Type;
-                    model = await _dbContext.LikeDislikes.Update(model);
-                    log.LogInformation("Updated reaction to " + model.Type);
-                } else {
-                    log.LogInformation("Reaction is the same. No change");
-                }
-            } else if (model == null) {
+            if (model == null)
+            {
                 data.Id = Guid.NewGuid().ToString();
                 model = await _dbContext.LikeDislikes.Add(data);
                 log.LogInformation("Added new reaction. Id: {0}", model.Id);
             }
+            else
+            {
+                log.LogInformation("Reaction already exists for user {0}", model.UserId);
+
+                if (model.Type != data.Type)
+                {
+                    model.Type = data.Type;
+                    model = await _dbContext.LikeDislikes.Update(model);
+                    log.LogInformation("Updated reaction to " + model.Type);
+                }
+                else
+                {
+                    log.LogInformation("Reaction is the same. No change");
+                }
+            }
 
             log.LogInformation($"User: {model.UserId}, Type: {model.Type}, Rule: {model.RuleGuid}, Id: {model.Id}");
 
-            return new JsonResult(new {
+            return new JsonResult(new
+            {
                 error = false,
                 message = "",
                 reaction = model.Type
