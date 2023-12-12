ce7c7942ae1901399738ca134af435f71e3ac204
JackDevAU <jpr178@live.com.au>
2023-12-12 10:30:14 +1000

    Fix Update Latest Rules

--- SSW.Rules.AzFuncs/Domain/FrontMatter.cs
+++ SSW.Rules.AzFuncs/Domain/FrontMatter.cs
@@ -1,8 +1,36 @@
+using YamlDotNet.Serialization;
+
 namespace SSW.Rules.AzFuncs.Domain;
 
 public class FrontMatter
 {
+    public string Type { get; set; }
+    
+    [YamlMember(Alias = "archivedreason", ApplyNamingConventions = false)]
+    public string ArchivedReason { get; set; }
+
     public string Title { get; set; }
-    public DateTime Created { get; set; }
+    public Guid Guid { get; set; }
     public string Uri { get; set; }
+    public DateTime Created { get; set; }
+    public List<Author> Authors { get; set; }
+    public List<string> Related { get; set; }
+    public List<string> Redirects { get; set; }
+
+    public FrontMatter()
+    {
+        Authors = new List<Author>();
+        Related = new List<string>();
+        Redirects = new List<string>();
+    }
+}
+
+public class Author
+{
+    public string Title { get; set; }
+    public string? Url { get; set; }
+    public string? Img { get; set; }
+    
+    [YamlMember(Alias = "noimage", ApplyNamingConventions = false)]
+    public bool? NoImage { get; set; }
 }
