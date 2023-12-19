09d84e4d275f17fba41c5a1049937cb0d28ca89f
JackDevAU <jpr178@live.com.au>
2023-12-19 23:18:47 +1000

    Add GITHUB_TOKEN secret

--- SSW.Rules.AzFuncs/Functions/Widget/GetLatestRules.cs
+++ SSW.Rules.AzFuncs/Functions/Widget/GetLatestRules.cs
@@ -21,7 +21,7 @@ public class GetLatestRules(ILoggerFactory loggerFactory, RulesDbContext context
         var query = HttpUtility.ParseQueryString(req.Url.Query);
         var skip = int.Parse(query["skip"] ?? "0");
         var take = int.Parse(query["take"] ?? "10");
-        var githubUsername = query["githubUsername"].Trim('\"');
+        var githubUsername = query["githubUsername"]?.Trim('\"') ?? String.Empty;
 
         _logger.LogInformation($"Fetching latest rules, Skip: {skip}, Take: {take}, GitHubUsername: {githubUsername}");
 
