17251818a7561e73cf15c89a9a8ec5a90d133a04
Brook Jeynes <jeynesbrook@gmail.com>
2024-03-04 13:19:52 +1000

    Filter out duplicate rules

--- SSW.Rules.AzFuncs/Functions/Widget/GetLatestRules.cs
+++ SSW.Rules.AzFuncs/Functions/Widget/GetLatestRules.cs
@@ -31,6 +31,7 @@ public class GetLatestRules(ILoggerFactory loggerFactory, RulesDbContext context
                         r.CreatedBy == githubUsername || r.UpdatedBy == githubUsername)
             .GroupBy(r => r.RuleGuid)
             .Select(group => group.First())
+            .DistinctBy(r => r.RuleGuid)
             .OrderByDescending(r => r.UpdatedAt)
             .Skip(skip)
             .Take(take);
