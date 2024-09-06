a3daa4994bfa4be2d34c44b83834f7d93a6d8c93
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-09-06 09:10:21 +0800

    made optional for dto as it was failing on build

--- SSW.Rules.AzFuncs/Domain/RuleHistoryData.cs
+++ SSW.Rules.AzFuncs/Domain/RuleHistoryData.cs
@@ -3,9 +3,9 @@ namespace SSW.Rules.AzFuncs.Domain;
 public class RuleHistoryData
 {
     public string file { get; set; }
-    public string title { get; set; }
-    public string uri { get; set; }
-    public bool isArchived { get; set; }
+    public string? title { get; set; }
+    public string? uri { get; set; }
+    public bool? isArchived { get; set; }
     public string lastUpdated { get; set; }
     public string lastUpdatedBy { get; set; }
     public string lastUpdatedByEmail { get; set; }
